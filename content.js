e9398c1030305024ff6e054cdcdbbe49d537dc83
unknown <chawnuhtun@gmail.com>
2026-04-21 00:46:02 +0630

    Added Text inside, icon

--- content.js
+++ content.js
@@ -1,12 +1,19 @@
 (function () {
   if (document.getElementById("my-extension-button")) return;
 
-  // === Create Sidebar ===
+  // === Sidebar container ===
   const sidebar = document.createElement("div");
   sidebar.id = "my-extension-sidebar";
   sidebar.style.display = "none";
 
-  // === Create Floating Button ===
+  // === Load HTML into sidebar ===
+  fetch(chrome.runtime.getURL("main.html"))
+    .then(res => res.text())
+    .then(html => {
+      sidebar.innerHTML = html;
+    });
+
+  // === Floating button ===
   const button = document.createElement("div");
   button.id = "my-extension-button";
 
@@ -14,7 +21,7 @@
   img.src = chrome.runtime.getURL("img/TruthLens.png");
   button.appendChild(img);
 
-  // === Toggle Logic ===
+  // === Toggle ===
   button.addEventListener("click", () => {
     sidebar.style.display =
       sidebar.style.display === "none" ? "block" : "none";
